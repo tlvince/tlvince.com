e2fc89af22396df01cd2cc229e3f1d0d5399693e
Tom Vincent <git@tlvince.com>
2012-09-16 12:34:50 +0800

    Add asset manager

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
-var express = require('express'),
-    app     = express(),
-    port    = process.env.PORT || 3000,
-    oneDay  = 86400000;
+var express      = require('express'),
+    assetManager = require('connect-assetmanager'),
+    app          = express(),
+    port         = process.env.PORT || 3000,
+    oneDay       = 86400000;
+
+var assetManagerGroups = {
+  css: {
+    route: /\/assets\/style\/.*\.css/,
+    path: __dirname + '/build/assets/style/',
+    dataType: 'css',
+    files: ['site.css']
+  }
+};
+
+var assetsManagerMiddleware = assetManager(assetManagerGroups);
 
 app.use(express.compress());
 app.use(express.logger('short'));
+app.use(assetsManagerMiddleware);
 app.use(express.favicon(__dirname + '/build/assets'));
 app.use(express.static(__dirname + '/build', { maxAge: oneDay }));
 
