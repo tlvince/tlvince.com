a6c6e0900240f436762457be4d1ae9126a50611b
Tom Vincent <git@tlvince.com>
2013-09-10 17:14:40 +0100

    Add workaround for wintersmith

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
-wintersmith="./node_modules/.bin/wintersmith"
+coffee="./node_modules/.bin/coffee"
+wintersmith="./node_modules/wintersmith"
 public="public"
 
 # Relative to public
 build="../build"
 
+# XXX: Needed to respect the new `filenameTemplate` config
+rm -rf "$wintersmith/lib"
+"$coffee" -o "$wintersmith/lib" -b -c "$wintersmith/src"
+
 [ -d "$build" ] && rm -rf "$build"
-"$wintersmith" build --output "$build" --chdir "$public"
+"$wintersmith/bin/wintersmith" build --output "$build" --chdir "$public"
