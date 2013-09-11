140d9898ee8e0b0be21635a1e1b0c55a3d35f56a
Tom Vincent <git@tlvince.com>
2013-09-11 10:55:20 +0100

    ws 2.0.7 fixes prepublish compilation

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
 
-coffee="./node_modules/.bin/coffee"
-wintersmith="./node_modules/wintersmith"
+wintersmith="./node_modules/wintersmith/bin/wintersmith"
 public="public"
 
 # Relative to public
 build="../build"
 
-# XXX: Needed to respect the new `filenameTemplate` config
-rm -rf "$wintersmith/lib"
-"$coffee" -o "$wintersmith/lib" -b -c "$wintersmith/src"
-
 [ -d "$build" ] && rm -rf "$build"
-"$wintersmith/bin/wintersmith" build --output "$build" --chdir "$public"
+"$wintersmith" build --output "$build" --chdir "$public"
